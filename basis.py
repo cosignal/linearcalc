caea041effe89f718b9ab240746fefad258e5bd9
cosignal <jjhansenhlebica@gmail.com>
2024-08-23 18:01:59 -0700

    Add file for code related to coordinate systems and change-of-basis

---
+++ basis.py
@@ -0,0 +1,9 @@
+"""
+Module Name: basis.py
+Description: This module contains functionality related to basis
+             vectors, coordinate systems, and change-of-basis.
+Author: James Hansen
+Created: 8/23/24
+"""
+
+# TODO
--- linearcalc.py
+++ linearcalc.py
@@ -1,9 +1,9 @@
 """
 Module Name: linearcalc.py
-Description: I linear algebra calculator library.
+Description: A linear algebra calculator library.
 Author: James Hansen
 Created: 8/23/24
 """
 
-# TODO: use this file for CLI code
+# TODO: use this file for client code
 
