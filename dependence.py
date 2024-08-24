1e8d174327916c7e3291177e0acb4bb7a4184298
cosignal <jjhansenhlebica@gmail.com>
2024-08-23 20:02:51 -0700

    Add file for dependence relations

---
+++ dependence.py
@@ -0,0 +1,11 @@
+"""
+Module Name: dependence.py
+Description: This module contains functionality for determining 
+             the column/row space (and rank), 
+             null space (and nullity), and linear independence of
+             the column/row vectors of a matrix.
+Author: James Hansen
+Created: 8/23/24
+"""
+
+# TODO: linear independence, rank, nullity, etc...
