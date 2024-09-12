c50a9ace474fff6137e9d53e3f38d53de5c8cf2c
cosignal <jjhansenhlebica@gmail.com>
2024-09-11 23:50:25 -0700

    Add functionality for finding eigenvectors of a matrix

--- eigenvectors.py
+++ eigenvectors.py
@@ -6,13 +6,13 @@ Author: James Hansen
 Created: 8/23/24
 """
 
-from sympy import Symbol, expand
+from sympy import Symbol, poly, expand
 from numpy import roots, round
-from numpy.polynomial import Polynomial
 from matrix_arithmetic import scale_matrix, subtract_matrices
 from inverse import identity
 from determinant import determinant
 from space import null_space
+from copy import deepcopy
 
 def characteristic(matrix):
     # returns a characteristic matrix corresponding to the given matrix
@@ -26,13 +26,13 @@ def characteristic(matrix):
     return subtract_matrices(m, lambda_identity)
 
 def singular(characteristic, eigenvalue):
-    # substitutes lambda with eigenvalue for characteristic matrix 
-    singular = characteristic.copy()
+    # substitutes lambda with eigenvalue for characteristic matrix
+    singular = deepcopy(characteristic)
     for row_idx in range(len(singular)):
         for col_idx in range(len(singular[0])):
             if (row_idx == col_idx):
                 entry = singular[row_idx][col_idx].subs('lambda',eigenvalue)
-                singular[row_idx][col_idx] = entry
+                singular[row_idx][col_idx] = entry 
     return singular
 
 def eigenvalues(matrix, is_characteristic=False):
@@ -43,16 +43,34 @@ def eigenvalues(matrix, is_characteristic=False):
     else:
         characteristic_matrix = characteristic(matrix)
 
-    expanded = Poly(expand(determinant(characteristic_matrix)))
+    expanded = poly(expand(determinant(characteristic_matrix)))
     coeffs = expanded.coeffs()
 
     if (coeffs[0] < 0):
         neg_expanded = -expanded
+
         coeffs = neg_expanded.coeffs()
 
-    # NOTE: rounding here due to floating point precision in np.coeffs()
-    # (this may just be a quick fix and worth revising)
-    eigenvalues = round(roots(coeffs), decimals=8)
+    # FIXME: this currently accounts for integer eigenvalues
+    eigenvalues = round(roots(coeffs),decimals=0).astype(int)
 
     return eigenvalues
-    
+
+def eigenvectors_dict(matrix):
+    # returns dictionary of eigenvalues mapped to their eigenvectors
+    eigenvectors = {}
+    characteristic_matrix = characteristic(matrix)
+    evals = eigenvalues(characteristic_matrix, True)
+
+    for eigenvalue in evals:
+        eigenvectors[eigenvalue] = null_space(singular(characteristic_matrix, eigenvalue))
+
+    return eigenvectors
+
+def eigenvectors(matrix):
+    # returns array of eigenvectors associated with matrix
+    eigenvectors = []
+    for solution_set in eigenvectors_dict(matrix).values():
+        for eigenvector in solution_set:
+            eigenvectors.append(eigenvector)
+    return eigenvectors
